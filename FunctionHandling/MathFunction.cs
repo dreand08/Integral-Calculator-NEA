90d4979564f8feebefde5e148f79f0d370c536d0
dreand <dreand@stpaulsschool.org.uk>
2025-09-05 15:20:26 +0100

    Added more custom mappings

--- FunctionHandling/MathFunction.cs
+++ FunctionHandling/MathFunction.cs
@@ -31,6 +31,7 @@ namespace Computer_Science_NEA.FunctionHandling
                 return Math.Log(args[0]);
             }); // Here we added ln, and threw an exception for invalid inputs. 
             //Now we just have to repeat for all functions.
+            //List of possible inputs: sin, cos, tan, arctan, arcsin, arcos, sinh, cosh, tanh, arcsinh, arcosh, artanh, e, ln, log10, sqrt, abs, 
             Parser.LocalFunctions.Add("sin", args => Math.Sin(args[0])); //Defined for all x.
             Parser.LocalFunctions.Add("cos", args => Math.Cos(args[0]));
             Parser.LocalFunctions.Add("tan", args => Math.Tan(args[0]));
@@ -42,6 +43,20 @@ namespace Computer_Science_NEA.FunctionHandling
                 }
                 return Math.Log10(args[0]);
             });
+            Parser.LocalFunctions.Add("abs", args => Math.Abs(args[0]));
+            Parser.LocalFunctions.Add("sqrt", args =>
+            {
+                if (args[0] < 0)
+                {
+                    throw new ArgumentException("Sqrt only takes x >= 0");
+                }
+                return Math.Sqrt(args[0]);
+            });
+            Parser.LocalFunctions.Add("exp", args => Math.Exp(args[0]));
+            Parser.LocalFunctions.Add("sinh", args => Math.Sinh(args[0]));
+            Parser.LocalFunctions.Add("cosh", args => Math.Cosh(args[0]));
+            Parser.LocalFunctions.Add("tanh", args => Math.Tanh(args[0]));
+            Parser.LocalFunctions.Add("arcsin", args => Math.Asin(args[0]));
         }
     }
 }
