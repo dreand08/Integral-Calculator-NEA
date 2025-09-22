899e429eedc4681548a7407dfd735a4b11d8a59b
dreand <dreand@stpaulsschool.org.uk>
2025-09-22 09:45:45 +0100

    Fixed the test issue, added more tests. Arctan2 is still failing so i'm testing why.

--- FunctionHandling/MathFunction.cs
+++ FunctionHandling/MathFunction.cs
@@ -14,18 +14,13 @@ namespace Computer_Science_NEA.FunctionHandling
         public string Expression { get; set; }
         public MathParser Parser { get; set; }
 
-        private static MathParser SharedParser;
-
-        static MathFunction()
-        {
-            SharedParser = new MathParser();
-            AddCustomMappings(SharedParser); //Uses a shared parser to initally add mappings so they aren't added more than once.
-        }
+        //I initially had a shared parser but i removed it because it was causing tests to affect one another.
         //Constructor
         public MathFunction(string expression)
         {
             Expression = expression;
-            Parser = SharedParser;
+            Parser = new MathParser();
+            AddCustomMappings(Parser);
             //We must add mappings since MathParser only knows basic arithmetic functions.
             //In practice we are teaching the parser what all these different functions mean.
         }
@@ -61,15 +56,15 @@ namespace Computer_Science_NEA.FunctionHandling
             {
                 parser.LocalFunctions.Add("sin", args => Math.Sin(args[0])); //Defined for all x.
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("cos"))
             {
                 parser.LocalFunctions.Add("cos", args => Math.Cos(args[0]));
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("tan"))
             {
                 parser.LocalFunctions.Add("tan", args => Math.Tan(args[0]));
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("log"))
             {
                 parser.LocalFunctions.Add("log", args =>
                 {
@@ -80,11 +75,11 @@ namespace Computer_Science_NEA.FunctionHandling
                     return Math.Log10(args[0]);
                 });
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("abs"))
             {
                 parser.LocalFunctions.Add("abs", args => Math.Abs(args[0]));
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("sqrt"))
             {
                 parser.LocalFunctions.Add("sqrt", args =>
                 {
@@ -95,102 +90,153 @@ namespace Computer_Science_NEA.FunctionHandling
                     return Math.Sqrt(args[0]);
                 });
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("exp"))
             {
                 parser.LocalFunctions.Add("exp", args => Math.Exp(args[0]));
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("sinh"))
             {
                 parser.LocalFunctions.Add("sinh", args => Math.Sinh(args[0]));
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("cosh"))
             {
                 parser.LocalFunctions.Add("cosh", args => Math.Cosh(args[0]));
             }
-            if (!parser.LocalFunctions.ContainsKey("sin"))
+            if (!parser.LocalFunctions.ContainsKey("tanh"))
             {
                 parser.LocalFunctions.Add("tanh", args => Math.Tanh(args[0]));
             }
-            parser.LocalFunctions.Add("arcsin", args =>
+            if (!parser.LocalFunctions.ContainsKey("arcsin"))
             {
-                if (args[0] > 1 || args[0] < -1)
+                parser.LocalFunctions.Add("arcsin", args =>
                 {
-                    throw new ArgumentException("Arcsin only takes -1<x<1");
-                }
-                return Math.Asin(args[0]);
-            });
-            parser.LocalFunctions.Add("arcos", args =>
+                    if (args[0] > 1 || args[0] < -1)
+                    {
+                        throw new ArgumentException("Arcsin only takes -1<x<1");
+                    }
+                    return Math.Asin(args[0]);
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("arcos"))
             {
-                if (args[0] > 1 || args[0] < -1)
+                parser.LocalFunctions.Add("arcos", args =>
                 {
-                    throw new ArgumentException("Arcos only takes -1<x<1");
-                }
-                return Math.Acos(args[0]);
-            });
-            parser.LocalFunctions.Add("arctan", args => Math.Atan(args[0]));
-            parser.LocalFunctions.Add("arcsinh", args => Math.Log(args[0] + Math.Sqrt(Math.Pow(args[0], 2) + 1)));
-            parser.LocalFunctions.Add("arcosh", args => // Arcosh, Arcsinh, Arctanh aren't defined in Math library so must be implemented manually.
+                    if (args[0] > 1 || args[0] < -1)
+                    {
+                        throw new ArgumentException("Arcos only takes -1<x<1");
+                    }
+                    return Math.Acos(args[0]);
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("arctan"))
             {
-                if (args[0] < 1)
-                {
-                    throw new ArgumentException("Arcosh only takes x >= 1");
-                }
-                return Math.Log(args[0] + Math.Sqrt(Math.Pow(args[0], 2) - 1));
-            });
-            parser.LocalFunctions.Add("arctanh", args =>
+                parser.LocalFunctions.Add("arctan", args => Math.Atan(args[0]));
+            }
+            if (!parser.LocalFunctions.ContainsKey("arcsinh"))
             {
-                if (args[0] <= -1 || args[0] >= 1)
-                {
-                    throw new ArgumentException("Arctanh only takes -1<x<1");
-                }
-                return 0.5 * Math.Log((1 + args[0]) / (1 - args[0]));
-            });
-            parser.LocalFunctions.Add("sec", args =>
+                parser.LocalFunctions.Add("arcsinh", args => Math.Log(args[0] + Math.Sqrt(Math.Pow(args[0], 2) + 1)));
+            }
+            if (!parser.LocalFunctions.ContainsKey("arcosh"))
             {
-                if (args[0] == 0)
+                parser.LocalFunctions.Add("arcosh", args => // Arcosh, Arcsinh, Arctanh aren't defined in Math library so must be implemented manually.
                 {
-                    throw new ArgumentException("Sec cannot take x = 0");
-                }
-                return 1 / Math.Cos(args[0]);
-            });
-            parser.LocalFunctions.Add("cosec", args =>
+                    if (args[0] < 1)
+                    {
+                        throw new ArgumentException("Arcosh only takes x >= 1");
+                    }
+                    return Math.Log(args[0] + Math.Sqrt(Math.Pow(args[0], 2) - 1));
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("arctanh"))
             {
-                if (args[0] == 0)
+                parser.LocalFunctions.Add("arctanh", args =>
                 {
-                    throw new ArgumentException("Cosec cannot take x = 0");
-                }
-                return 1 / Math.Sin(args[0]);
-            });
-            parser.LocalFunctions.Add("cot", args =>
+                    if (args[0] <= -1 || args[0] >= 1)
+                    {
+                        throw new ArgumentException("Arctanh only takes -1<x<1");
+                    }
+                    return 0.5 * Math.Log((1 + args[0]) / (1 - args[0]));
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("sec"))
             {
-                if (args[0] == 0)
+                parser.LocalFunctions.Add("sec", args =>
                 {
-                    throw new ArgumentException("Cot cannot take x = 0");
-                }
-                return 1 / Math.Tan(args[0]);
-            });
-            parser.LocalFunctions.Add("floor", args => Math.Floor(args[0]));
-            parser.LocalFunctions.Add("ceil", args => Math.Ceiling(args[0]));
-            parser.LocalFunctions.Add("round", args => Math.Round(args[0]));
-            parser.LocalFunctions.Add("sign", args => Math.Sign(args[0])); // Returns sign of argument
-            parser.LocalFunctions.Add("min", args => args.Min()); // This allows for 1 or more values in the min/max functions.
-            parser.LocalFunctions.Add("max", args => args.Max());
-            parser.LocalFunctions.Add("pow", args =>
+                    if (Math.Cos(args[0]) == 0)
+                    {
+                        throw new ArgumentException("Sec cannot take cosx = 0");
+                    }
+                    return 1 / Math.Cos(args[0]);
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("cosec"))
             {
-                if (args.Length != 2)
+                parser.LocalFunctions.Add("cosec", args =>
                 {
-                    throw new ArgumentException("Please input a value and its power in the form pow(x,y)");
-                }
-                return Math.Pow(args[0], args[1]);
-            });
-            parser.LocalFunctions.Add("arctan2", args =>
+                    if (args[0] == 0)
+                    {
+                        throw new ArgumentException("Cosec cannot take x = 0");
+                    }
+                    return 1 / Math.Sin(args[0]);
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("cot"))
             {
-                if (args.Length != 2)
+                parser.LocalFunctions.Add("cot", args =>
                 {
-                    throw new ArgumentException("Please input an x and y value for finding the arctan of x/y measured from x-axis.");
-                }
-                return Math.Atan2(args[0], args[1]);
-            });
+                    if (args[0] == 0)
+                    {
+                        throw new ArgumentException("Cot cannot take x = 0");
+                    }
+                    return 1 / Math.Tan(args[0]);
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("floor"))
+            {
+                parser.LocalFunctions.Add("floor", args => Math.Floor(args[0]));
+            }
+            if (!parser.LocalFunctions.ContainsKey("ceil"))
+            {
+                parser.LocalFunctions.Add("ceil", args => Math.Ceiling(args[0]));
+            }
+            if (!parser.LocalFunctions.ContainsKey("round"))
+            {
+                parser.LocalFunctions.Add("round", args => Math.Round(args[0]));
+            }
+            if (!parser.LocalFunctions.ContainsKey("sign"))
+            {
+                parser.LocalFunctions.Add("sign", args => Math.Sign(args[0])); // Returns sign of argument
+            }
+            if (!parser.LocalFunctions.ContainsKey("min"))
+            {
+                parser.LocalFunctions.Add("min", args => args.Min()); // This allows for 1 or more values in the min/max functions.
+            }
+            if (!parser.LocalFunctions.ContainsKey("max"))
+            {
+                parser.LocalFunctions.Add("max", args => args.Max());
+            }
+            if (!parser.LocalFunctions.ContainsKey("pow"))
+            {
+                parser.LocalFunctions.Add("pow", args =>
+                {
+                    if (args.Length != 2)
+                    {
+                        throw new ArgumentException("Please input a value and its power in the form pow(x,y)");
+                    }
+                    return Math.Pow(args[0], args[1]);
+                });
+            }
+            if (!parser.LocalFunctions.ContainsKey("arctan2"))
+            {
+                parser.LocalFunctions.Add("arctan2", args =>
+                {
+                    if (args.Length != 2)
+                    {
+                        throw new ArgumentException("Please input an x and y value for finding the arctan of x/y measured from x-axis.");
+                    }
+                    return Math.Atan2(args[1], args[0]); // y/x
+                });
+            }
         }
     }
 }
