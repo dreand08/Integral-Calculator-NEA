98c6baa4150c5d43a00fbcd1b44fafb880d0f9ce
dreand <dreand@stpaulsschool.org.uk>
2025-10-01 09:45:44 +0100

    Invalid tests still mostly failing, trying to fix but everything else works. The function parser is almost complete.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace Computer_Science_NEA
     {
         static void Main(string[] args)
         {
-            var f = new MathFunction("sqrt(max(y ,abs(sec(x * floor(a)) * arctan(2 + 9 * arctanh(min(ln(sin(a)), pow(pow(x,y),z))))))))");
-            var inputs = new Dictionary<string, double> { {"a", Math.PI / 2 },{ "x", 3 }, { "y", 8 }, { "z", 2 } };
+            var f = new MathFunction("");
+            var inputs = new Dictionary<string, double> { };
             var result = f.Evaluate(inputs);
             Console.WriteLine(result);
         }
