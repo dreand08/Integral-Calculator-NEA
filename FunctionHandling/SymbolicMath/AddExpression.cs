79737c4b7b423ee9e49c018722c8cb9b0fecdc03
dreand <dreand@stpaulsschool.org.uk>
2025-11-26 09:24:08 +0000

    Finally got back to work, had some struggle implementing the make class but managed to get throught it. Almost done with the addition class.

--- FunctionHandling/SymbolicMath/AddExpression.cs
+++ FunctionHandling/SymbolicMath/AddExpression.cs
@@ -26,5 +26,64 @@ namespace Computer_Science_NEA.FunctionHandling.SymbolicMath
 
         private static decimal GetNumber(Expression e) =>
             e is NumberExpression n ? n.Value : 0m;
+
+        public static Expression Make(params Expression[] rawTerms)
+        {
+            var nonConst = new List<Expression>();
+            decimal constSum = 0m;
+
+            //1. Flatten and split constants from non constants
+            foreach(var t in rawTerms)
+            {
+                if (t is AddExpression add)
+                {
+                    foreach (var sub in add.Terms)
+                    {
+                        if (IsNumber(sub)) constSum += GetNumber(sub);
+                        else nonConst.Add(sub);
+                    }
+                }
+                else if (IsNumber(t))
+                {
+                    constSum += GetNumber(t);
+                }
+                else
+                {
+                    nonConst.Add(t);
+                }
+            }
+
+            //2. Drop +0
+            var result = new List<Expression>();
+            foreach (var t in nonConst)
+            {
+                if (!IsZero(t)) result.Add(t);
+            }
+
+            if (constSum != 0m || result.Count == 0)
+            {
+                result.Add(new NumberExpression(constSum));
+            }
+
+            //3. Sort for consistent printing.
+
+            result = result.OrderBy(t => t.GetType().Name).ThenBy(t => t.ToString()).ToList();
+
+            //4. if only 1 term, return it directly.
+            if (result.Count == 1)
+            {
+                return result[0];
+            }
+
+            return new AddExpression(result);
+        }
+
+        public override Expression Simplify()
+        {
+            var simplified = Terms.Select(t => t.Simplify()).ToArray();
+            return Make(simplified);
+        }
+
+        public override Expression Substitute(Expression target)
     }
 }
