dc150bed13649a118ce7874b21a76224a51491a4
dreand <dreand@stpaulsschool.org.uk>
2026-01-09 13:23:45 +0000

    Made some small progess on power expression classes constructing the base method overrides

--- FunctionHandling/SymbolicMath/PowerExpression.cs
+++ FunctionHandling/SymbolicMath/PowerExpression.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Computer_Science_NEA.FunctionHandling.SymbolicMath
 {
-    class PowerExpression
+    public sealed class PowerExpression : Expression
     {
+        public Expression BaseExpr { get; }
+        public Expression Exponent { get; }
+
+        private PowerExpression(Expression baseExpr, Expression exponent)
+        {
+            BaseExpr = baseExpr;
+            Exponent = exponent;
+        }
+
+        public override int Precedence => 40; // Higher than multiply
+
+        private static bool IsNumber(Expression e, out decimal v)
+        {
+            if (e is NumberExpression n)
+            {
+                v = n.Value;
+                return true;
+            }
+            v = 0m;
+            return false;
+        }
+
+        private static bool IsZero(Expression e) =>
+            IsNumber(e, out var v) && v == 0m;
+
+        private static bool IsOne(Expression e) =>
+            IsNumber(e, out var v) && v == 1m;
+
+        public static Expression Make(Expression b, Expression e)
+        {
+            if (IsZero(b)) 
+        }
     }
 }
